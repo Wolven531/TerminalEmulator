14c8b41c6e812e64fe8d5b3895e22c3599cd6e3d
Anthony Williams <anthony.williams.cs@gmail.com>
2025-06-04 01:03:59 -0700

    Add test config for vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	test: {
+		browser: {
+			enabled: true,
+			instances: [{ browser: 'chromium' }],
+			provider: 'playwright',
+			ui: true,
+		},
+		coverage: {
+			enabled: true,
+			exclude: ['src/main.tsx', '**/*.d.ts', '**/*.config.{js,ts}'],
+			thresholds: {
+				branches: 75,
+				functions: 75,
+				lines: 75,
+				statements: 75,
+			},
+		},
+		globals: true,
+	},
 })
