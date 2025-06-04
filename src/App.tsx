0af5bf8a13876018ee762830fc6ca8c1d1cd4c32
Anthony Williams <anthony.williams.cs@gmail.com>
2025-06-04 01:08:44 -0700

    Display TerminalEmulator in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { type FC } from 'react'
+import { TerminalEmulator } from './components/TerminalEmulator'
 
 // styles
 import './App.css'
@@ -7,6 +8,7 @@ export const App: FC = () => {
 	return (
 		<>
 			<h3>App</h3>
+			<TerminalEmulator />
 		</>
 	)
 }
