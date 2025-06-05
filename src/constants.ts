e09665e5df2c2d3d9e6e6c4fe2a32f7a6fd8ba05
Anthony Williams <anthony.williams.cs@gmail.com>
2025-06-04 20:07:21 -0700

    Add constants

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+/**
+ * Default time (milliseconds) to wait before next line starts render
+ */
+export const DEFAULT_DELAY_BETWEEN_LINES = 0
+/**
+ * Default time (milliseconds) to wait before next character starts render
+ */
+export const DEFAULT_DELAY_PER_CHARACTER = 50
